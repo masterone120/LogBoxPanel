ad96fc8d8ca1e37ce03fc7b11164ca4bf5c4964d
masterone120 <emami.javad91@gmail.com>
2024-09-18 08:05:09 -0400

    v1.0.2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './Router';
 import './index.css';
-import { AuthProvider} from "./components/AuthContext.jsx";
+import { AuthProvider} from "./components/AuthContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
